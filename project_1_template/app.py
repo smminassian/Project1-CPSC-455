decf26225d8465af369646d9863fb674ad6908aa
smminassian <143156014+smminassian@users.noreply.github.com>
2025-10-03 13:06:42 -0700

    now im on the wireshark capture. We take a break and come back later

--- project_1_template/app.py
+++ project_1_template/app.py
@@ -12,9 +12,6 @@ def index():
 
 @app.route('/info')
 def info():
-    # TODO: add your info.html call. 
-    # A sample info.html file is already included. Please check info.html file and add your information
-    # in that page as well.
     return render_template('info.html')
 
 if __name__ == '__main__':
